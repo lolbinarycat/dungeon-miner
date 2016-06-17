01fc5ab4df636707f0770c7d885900cb80dc0285
lolbinarycat <dogedoge61+github@gmail.com>
2016-06-17 12:29:42 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Make an instance of two and place it on the page.
+var elem = document.getElementById('main');
+var params = { width: 285, height: 200 };
+var two = new Two(params).appendTo(elem);
+
+//player constructor
+function player(); {
+  this.inventory = [];
+  this.body =  = two.makeRectangle(70, 0, 100, 100); //part of the player that can be saw
+}
+
